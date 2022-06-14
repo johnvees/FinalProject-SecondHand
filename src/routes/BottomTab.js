5d48a2c707b5dcb0badfd9091e1deba1dba6a07d
cemilick <cemilick@gmail.com>
2022-06-14 22:30:02 +0700

    add icon on bottom tab navigator

--- src/routes/BottomTab.js
+++ src/routes/BottomTab.js
@@ -6,17 +6,91 @@ import Akun from '../screens/Akun';
 import DaftarJual from '../screens/DaftarJual';
 import Jual from '../screens/Jual';
 import Notifikasi from '../screens/Notifikasi';
+import Feather from 'react-native-vector-icons/Feather';
+import {ms} from 'react-native-size-matters';
 
 const BottomTab = () => {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Akun" component={Akun} />
-      <Tab.Screen name="DaftarJual" component={DaftarJual} />
-      <Tab.Screen name="Jual" component={Jual} />
-      <Tab.Screen name="Notifikasi" component={Notifikasi} />
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{headerShown: false, tabBarActiveTintColor: '#7126B5'}}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: tabInfo => {
+            return (
+              <Feather
+                name="home"
+                size={ms(24)}
+                color={tabInfo.focused ? '#7126B5' : '#8A8A8A'}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Notifikasi"
+        component={Notifikasi}
+        options={{
+          tabBarIcon: tabInfo => {
+            return (
+              <Feather
+                name="bell"
+                size={ms(24)}
+                color={tabInfo.focused ? '#7126B5' : '#8A8A8A'}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Jual"
+        component={Jual}
+        options={{
+          tabBarIcon: tabInfo => {
+            return (
+              <Feather
+                name="plus-circle"
+                size={ms(24)}
+                color={tabInfo.focused ? '#7126B5' : '#8A8A8A'}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="DaftarJual"
+        component={DaftarJual}
+        options={{
+          tabBarIcon: tabInfo => {
+            return (
+              <Feather
+                name="list"
+                size={ms(24)}
+                color={tabInfo.focused ? '#7126B5' : '#8A8A8A'}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Akun"
+        component={Akun}
+        options={{
+          tabBarIcon: tabInfo => {
+            return (
+              <Feather
+                name="user"
+                size={ms(24)}
+                color={tabInfo.focused ? '#7126B5' : '#8A8A8A'}
+              />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
