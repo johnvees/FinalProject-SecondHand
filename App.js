58befc1fe31a0cb5041d63238346d0b9b031e664
cemilick <cemilick@gmail.com>
2022-06-14 21:19:45 +0700

    config stack navigator on app.js

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const App = () => {
+  const Stack = createStackNavigator();
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <Stack.Navigator>
+      <BottomTab />
+    </Stack.Navigator>
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
