82973ea51077ba2846009b37a59b56450c7ebdc4
cemilick <cemilick@gmail.com>
2022-06-14 21:31:44 +0700

    import BottomTab on App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-
+import BottomTab from './src/routes/BottomTab';
 const App = () => {
   const Stack = createStackNavigator();
 
