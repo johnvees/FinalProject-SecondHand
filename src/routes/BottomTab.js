87f4e0347f62806109ba52d2fbccbfe8e1b40208
cemilick <cemilick@gmail.com>
2022-06-14 21:52:39 +0700

    add headerShown: false on BottomTab

--- src/routes/BottomTab.js
+++ src/routes/BottomTab.js
@@ -6,7 +6,7 @@ const BottomTab = () => {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator initialRouteName="Home">
+    <Tab.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
       <Tab.Screen name="Home" component={Home} />
     </Tab.Navigator>
   );
