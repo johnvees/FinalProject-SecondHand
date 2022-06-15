67d3ba5e27b49acbc20ab4bf1c25e8cea27a02e4
AzhamDev <dev.azham@gmail.com>
2022-06-15 21:02:18 +0700

    Add Global Fonts OpenSans/Poppins

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import { ms } from 'react-native-size-matters'
+import { MyFonts } from './src/utils/fonts'
+
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+      <Text style={{ fontSize: ms(30), fontFamily: MyFonts.Regular }}>Test Fonts</Text>
+      <Text style={{ fontSize: ms(30), fontFamily: MyFonts.Bold }}>Test Fonts</Text>
+      <Text style={{ fontSize: ms(30), fontFamily: MyFonts.Medium }}>Test Fonts</Text>
     </View>
   )
 }
---
+++ src/utils/fonts/index.js
@@ -0,0 +1,5 @@
+export const MyFonts = {
+    Regular: 'OpenSans-Regular',
+    Medium: 'OpenSans-Medium',
+    Bold: 'OpenSans-Bold'
+}
