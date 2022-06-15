7e78052e611ffb01db0541d315b8813187008a0b
johnvees <yohanesvelly23@gmail.com>
2022-06-15 15:43:44 +0700

    add hooks logic for password visibility, add base url for call api, and add logic for post login

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -5,14 +5,40 @@ import {
   TouchableOpacity,
   TextInput,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {ms} from 'react-native-size-matters';
+import axios from 'axios';
 
-const Home = () => {
+import {BASE_URL} from './src/utils';
+
+const Home = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const postLogin = async () => {
+    try {
+      const body = {
+        email: email,
+        password: password,
+      };
+
+      const result = await axios.post(`${BASE_URL}/auth/login`, body);
+
+      if (result.status === 201) {
+        console.log(result);
+        // menambahkan navigation.replace
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity style={styles.backButtonContainer}>
+      <TouchableOpacity
+        style={styles.backButtonContainer}
+        onPress={() => navigation.goBack()}>
         <View style={styles.backButton}></View>
       </TouchableOpacity>
       <Text style={styles.loginTitle}>Masuk</Text>
@@ -24,19 +50,28 @@ const Home = () => {
           placeholderTextColor={'#8A8A8A'}
           selectionColor={'#7126B5'}
           keyboardType="email-address"
+          onChangeText={text => {
+            setEmail(text);
+          }}
         />
       </View>
+      <Text style={styles.inputLabel}>Password</Text>
       <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Password</Text>
         <TextInput
           style={styles.textInput}
           placeholder="Masukkan password"
           placeholderTextColor={'#8A8A8A'}
           selectionColor={'#7126B5'}
           secureTextEntry={true}
+          onChangeText={text => {
+            setPassword(text);
+          }}
         />
+        <TouchableOpacity style={styles.passwordContainer}>
+          <View style={styles.eyeButton}></View>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.buttonLogin}>
+      <TouchableOpacity style={styles.buttonLogin} onPress={postLogin}>
         <Text style={styles.buttonLoginTitle}>Masuk</Text>
       </TouchableOpacity>
       <View style={styles.secondaryButtonContainer}>
@@ -73,7 +108,7 @@ const styles = StyleSheet.create({
     color: '#000000',
     marginBottom: ms(24),
   },
-  inputContainer: {marginBottom: ms(16)},
+  inputContainer: {marginBottom: ms(16), justifyContent: 'center'},
   inputLabel: {
     fontSize: ms(12),
     color: '#000000',
@@ -88,6 +123,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: ms(16),
     color: '#000000',
     fontSize: ms(14),
+  },
+  passwordContainer: {position: 'absolute', right: ms(10)},
+  eyeButton: {
+    height: ms(24),
+    width: ms(24),
+    backgroundColor: '#8A8A8A',
   },
   buttonLogin: {
     paddingHorizontal: ms(24),
