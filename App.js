96dbb343a49be8cc02131ec349380726efc27117
Suhendanifs <94581657+Suhendanifs@users.noreply.github.com>
2022-06-20 19:46:10 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import Biometric from './src/component/bio-auth';
 
 const App = () => {
   return (
     <View>
       <Text>App</Text>
-      <Biometric />
     </View>
   );
 };
