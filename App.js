30c3a614a6eeb8fbba92c6843cbf04347f4ca647
AzhamDev <dev.azham@gmail.com>
2022-06-22 23:06:54 +0700

    Done : Text Input

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Input from './src/components/TextInput'
+import Gap from './src/components/Gap'
+import { ms } from 'react-native-size-matters'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{ paddingHorizontal: ms(16) }}>
+      <Input title={"Email"} placeholder={"Contoh: johndee@gmail.com"} />
+      <Gap height={ms(16)} />
+      <Input secure={true} title={"Password"} placeholder={"Masukkan password"} />
     </View>
   )
 }
