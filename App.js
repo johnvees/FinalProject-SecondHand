3e6cb9ab2c61f49a1c7df90c570f631ea9284f0a
cemilick <cemilick@gmail.com>
2022-06-23 21:40:18 +0700

    add Login and Register to Stack Navigator and add initialRouteName Login

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import {createStackNavigator} from '@react-navigation/stack';
 import BottomTab from './src/routes/BottomTab';
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
 import {NavigationContainer} from '@react-navigation/native';
 const App = () => {
   const Stack = createStackNavigator();
@@ -13,7 +15,11 @@ const App = () => {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Navigator
+        screenOptions={{headerShown: false}}
+        initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="BottomTab" component={BottomTab} />
       </Stack.Navigator>
     </NavigationContainer>
