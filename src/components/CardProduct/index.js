a8cabeb18d1e7db34017c327730f8c80ca52571d
cemilick <cemilick@gmail.com>
2022-06-23 21:23:15 +0700

    fix card product and fix App.js on double configuration navigation

--- src/components/CardProduct/index.js
+++ src/components/CardProduct/index.js
@@ -2,16 +2,17 @@ import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import {ms} from 'react-native-size-matters';
 
-const Index = (
-  source = 'https://www.freeiconspng.com/uploads/no-image-icon-13.png',
-  product_name = 'Product Name',
+const Index = ({
+  source = 'https://www.freeiconspng.com/uploads/no-image-icon-11.PNG',
+  productName = 'Product Name',
   category = 'Category',
   price = 250000,
-) => {
+  id,
+}) => {
   return (
-    <TouchableOpacity style={styles.cardContainer}>
-      <Image source={{uri: source}} style={styles.image} resizeMode="contain" />
-      <Text style={styles.product_name}>{product_name}</Text>
+    <TouchableOpacity style={styles.cardContainer} onPress={() => {}}>
+      <Image source={{uri: source}} style={styles.image} resizeMode="cover" />
+      <Text style={styles.product_name}>{productName}</Text>
       <Text style={styles.category}>{category}</Text>
       <Text style={styles.price}>Rp. {price}</Text>
     </TouchableOpacity>
@@ -28,19 +29,23 @@ const styles = StyleSheet.create({
     borderRadius: ms(4),
     padding: ms(8),
     alignItems: 'flex-start',
+    elevation: ms(4),
   },
   image: {
     width: ms(140),
     height: ms(100),
     borderRadius: ms(4),
+    marginBottom: ms(8),
   },
   product_name: {
     fontSize: ms(14),
     color: '#151515',
+    marginBottom: ms(4),
   },
   category: {
     fontSize: ms(10),
     color: '#8A8A8A',
+    marginBottom: ms(8),
   },
   price: {
     fontSize: ms(14),
