5ca7e6ae1911d4155efce2c29128318a7c843d8f
Yohanes Velly Sabattino <yohanesvelly23@gmail.com>
2022-06-26 20:22:50 +0700

    change to new splash screen image with app icon

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react'
+import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import {createStackNavigator} from '@react-navigation/stack';
-import BottomTab from './src/routes/BottomTab';
 import {NavigationContainer} from '@react-navigation/native';
+
+import BottomTab from './src/routes/BottomTab';
+
 const App = () => {
   const Stack = createStackNavigator();
-  
+
   useEffect(() => {
     SplashScreen.hide();
   }, []);
-  
+
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <BottomTab />
-    </Stack.Navigator>
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="BottomTab" component={BottomTab} />
