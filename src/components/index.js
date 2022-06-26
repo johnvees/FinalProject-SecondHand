232954605bdd6ad394c134eb3ba732dca5bc5cd8
Yohanes Velly Sabattino <yohanesvelly23@gmail.com>
2022-06-27 01:33:54 +0700

    configure status bar component and make example

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import NumberFormat from './NumberFormat';
 import Button from './Button';
 import Biometric from './Biometric';
 import CardProduct from './CardProduct';
+import StatusBar from './StatusBar';
 
-export {NumberFormat, Button, Biometric, CardProduct};
+export {NumberFormat, Button, Biometric, CardProduct, StatusBar};
