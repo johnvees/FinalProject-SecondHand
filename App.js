a769cc4d66e574a56b8fe7ef1e5711648e0db2a9
Suhendanifs <danicavrep@gmail.com>
2022-06-27 20:38:10 +0700

    push ulang

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import BotSheet from './src/components/bottom-sheet';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <View>
       <Text>App</Text>
+      <BotSheet />
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
