3b31d562c363054cd32b1fcede19495050b62d08
cemilick <cemilick@gmail.com>
2022-07-04 20:21:35 +0700

    add props and style parameter on text input

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,47 +1,55 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native'
-import React, { useState } from 'react'
-import { ms } from 'react-native-size-matters'
-import Gap from '../Gap'
+import {StyleSheet, Text, View, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {ms} from 'react-native-size-matters';
+import Gap from '../Gap';
 
+export default function Input({
+  title,
+  secure,
+  value,
+  onChangeText,
+  placeholder,
+  style,
+  props = {},
+}) {
+  const [border, setBorder] = useState('#D0D0D0');
+  const onFocusForm = () => {
+    setBorder('#7126B5');
+  };
 
-export default function Input({ title, secure, value, onChangeText, placeholder }) {
-    const [border, setBorder] = useState("#D0D0D0")
-    const onFocusForm = () => {
-        setBorder("#7126B5")
-    }
-
-    const onBlurForm = () => {
-        setBorder("#D0D0D0")
-    }
-    return (
-        <View>
-            <Text style={styles.text}>{title}</Text>
-            <Gap height={ms(4)} />
-            <TextInput
-                onFocus={onFocusForm}
-                onBlur={onBlurForm}
-                style={styles.input(border)}
-                secureTextEntry={secure}
-                value={value}
-                onChangeText={onChangeText}
-                placeholder={placeholder}
-            />
-        </View>
-    )
+  const onBlurForm = () => {
+    setBorder('#D0D0D0');
+  };
+  return (
+    <View>
+      <Text style={styles.text}>{title}</Text>
+      <Gap height={ms(4)} />
+      <TextInput
+        {...props}
+        onFocus={onFocusForm}
+        onBlur={onBlurForm}
+        style={[styles.input(border), style]}
+        secureTextEntry={secure}
+        value={value}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    input: (border) => ({
-        borderRadius: ms(10),
-        borderWidth: ms(1),
-        borderColor: border,
-        paddingHorizontal: ms(16),
-        paddingVertical: ms(14),
-        color: "#8A8A8A"
-    }),
-    text: {
-        color: '#000',
-        fontSize: ms(14),
-        lineHeight: ms(20)
-    }
-})
+  input: border => ({
+    borderRadius: ms(10),
+    borderWidth: ms(1),
+    borderColor: border,
+    paddingHorizontal: ms(16),
+    paddingVertical: ms(14),
+    color: '#8A8A8A',
+  }),
+  text: {
+    color: '#000',
+    fontSize: ms(14),
+    lineHeight: ms(20),
+  },
+});
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -6,42 +6,69 @@ import {MyColors} from '../../utils/colors/index';
 import {MyFonts} from '../../utils';
 import Button from '../../components/Button';
 import {ms} from 'react-native-size-matters';
-
+import LinearGradient from 'react-native-linear-gradient';
+import {
+  heightPercentageToDP,
+  widthPercentageToDP,
+} from 'react-native-responsive-screen';
 const Index = () => {
   const [keyword, setKeyword] = useState('');
 
   return (
     <View>
-      <View>
-        <TextInput
-          name="search"
-          style={styles.textInput}
-          placeholder="Cari di Second Chance"
-          placeholderTextColor={MyColors.Neutral.NEUTRAL03}
-          selectionColor={MyColors.Primary.DARKBLUE04}
-          value={keyword}
-        />
-      </View>
-      <View>
-        <Text style={{fontStyles: MyFonts.Regular, fontSize: ms(20)}}>
-          Bulan Ramadhan Banyak diskon!
+      <LinearGradient
+        start={{x: 0, y: 0}}
+        end={{x: 0, y: 0.4}}
+        colors={['#FFE9C9', '#FFF']}
+        style={{
+          width: widthPercentageToDP(100),
+          height: heightPercentageToDP(100),
+        }}>
+        <View style={{alignSelf: 'center'}}>
+          <TextInput
+            name="search"
+            style={{
+              backgroundColor: '#FFF',
+              placeholderTextColor: '#000',
+              fontSize: ms(14),
+              fontFamily: MyFonts.Regular,
+              height: ms(48),
+              width: ms(328),
+              borderRadius: ms(16),
+            }}
+            props={{placeholderTextColor: '#8A8A8A'}}
+            placeholder="Cari di Second Chance"
+            placeholderTextColor={MyColors.Neutral.NEUTRAL03}
+            selectionColor={MyColors.Primary.DARKBLUE04}
+            value={keyword}
+            onChangeText={text => setKeyword(text)}
+          />
+        </View>
+        <View>
+          <Text style={{fontFamily: MyFonts.Bold, fontSize: ms(20)}}>
+            Bulan Ramadhan Banyak diskon!
+          </Text>
+          <Text style={{fontFamily: MyFonts.Bold, fontSize: ms(10)}}>
+            Diskon Hingga
+          </Text>
+          <Text
+            style={{
+              fontFamily: MyFonts.Regular,
+              color: 'red',
+              fontSize: ms(18),
+            }}>
+            60%
+          </Text>
+        </View>
+        <Text style={{fontStyles: MyFonts.Regular, fontSize: ms(14)}}>
+          Telusuri Kategori
         </Text>
-        <Text style={{fontStyles: MyFonts.Regular, fontSize: ms(10)}}>
-          Diskon Hingga
-        </Text>
-        <Text
-          style={{fontStyles: MyFonts.Regular, color: 'red', fontSize: ms(18)}}>
-          60%
-        </Text>
-      </View>
-      <Text style={{fontStyles: MyFonts.Regular, fontSize: ms(14)}}>
-        Telusuri Kategori
-      </Text>
-      <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-        <Button type="ctaFilter" filterText={'Semua'} />
-        <Button type="ctaFilter" filterText={'Hobi'} />
-        <Button type="ctaFilter" filterText={'Kendaraan'} />
-      </View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+          <Button type="ctaFilter" filterText={'Semua'} />
+          <Button type="ctaFilter" filterText={'Hobi'} />
+          <Button type="ctaFilter" filterText={'Kendaraan'} />
+        </View>
+      </LinearGradient>
     </View>
   );
 };
