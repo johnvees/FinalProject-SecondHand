6f7d41114cb1beccebbffff570b51c6854ee3a36
Yohanes Velly Sabattino <yohanesvelly23@gmail.com>
2022-07-04 23:58:30 +0700

    change dropdown library and styling ubah akun screen (undone)

--- src/screens/UbahAkun/index.js
+++ src/screens/UbahAkun/index.js
@@ -1,35 +1,36 @@
-/* eslint-disable no-undef */
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
   SafeAreaView,
   StatusBar,
-  Dimensions,
   StyleSheet,
-  ScrollView,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-const {width} = Dimensions.get('window');
-import SelectDropdown from 'react-native-select-dropdown';
+import {Dropdown} from 'react-native-element-dropdown';
 import axios from 'axios';
-import {BASE_URL_DAERAH} from '../../utils';
+import {Formik} from 'formik';
+import * as yup from 'yup';
+import {ms} from 'react-native-size-matters';
+import Feather from 'react-native-vector-icons/Feather';
 
-export default UbahAkun = () => {
+import {BASE_URL_DAERAH, MyColors, MyFonts} from '../../utils';
+import {Gap} from '../../components';
+
+export default UbahAkun = ({navigation}) => {
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
   const [provinsi, setProvinsi] = useState([]);
   const [kota, setKota] = useState([]);
   const [id, setId] = useState('');
-
-  const kotaDropdownRef = useRef();
 
   const getNamaProvinsi = async () => {
     try {
       const result = await axios.get(`${BASE_URL_DAERAH}/provinsi`);
       setProvinsi(result.data.provinsi);
       console.log(result.data.provinsi);
-      const res = await axios.get(
-        `${BASE_URL_DAERAH}/kota?id_provinsi=${id}`,
-      );
+      const res = await axios.get(`${BASE_URL_DAERAH}/kota?id_provinsi=${id}`);
       setKota(res.data.kota_kabupaten);
       console.log(res.data.kota_kabupaten);
     } catch (error) {
@@ -41,142 +42,227 @@ export default UbahAkun = () => {
     getNamaProvinsi();
   }, [id]);
 
-  const renderHeader = () => {
-    return (
-      <View style={[styles.header, styles.shadow]}>
-        <Text style={styles.headerTitle}>{'Demo 2'}</Text>
-      </View>
-    );
-  };
+  const putAccountValidationSchema = yup.object().shape({
+    fullname: yup.string().required('Nama Lengkap Dibutuhkan'),
+    address: yup.string().required('Alamat Dibutuhkan'),
+    phone_number: yup
+      .string()
+      .required('Nomor Handphone Dibutuhkan')
+      .min(10, 'Nomor Handphone Terlalu Pendek')
+      .max(14, 'Nomor Handphone Terlalu Panjang'),
+  });
 
   return (
-    <SafeAreaView style={styles.saveAreaViewContainer}>
-      <StatusBar backgroundColor="#FFF" barStyle="dark-content" />
-      <View style={styles.viewContainer}>
-        {renderHeader()}
-        <ScrollView
-          showsVerticalScrollIndicator={false}
-          alwaysBounceVertical={false}
-          contentContainerStyle={styles.scrollViewContainer}>
-          <View style={styles.dropdownsRow}>
-            <SelectDropdown
-              data={provinsi}
-              onSelect={(selectedItem, index) => {
-                console.log(selectedItem, index);
-                kotaDropdownRef.current.reset();
-                setId(selectedItem.id);
-                console.log(selectedItem.id);
-              }}
-              defaultButtonText={'Pilih Provinsi'}
-              buttonTextAfterSelection={(selectedItem, index) => {
-                return selectedItem.nama;
-              }}
-              rowTextForSelection={(item, index) => {
-                return item.nama;
-              }}
-              buttonStyle={styles.dropdown1BtnStyle}
-              buttonTextStyle={styles.dropdown1BtnTxtStyle}
-              renderDropdownIcon={isOpened => {
-                return (
-                  <FontAwesome
-                    name={isOpened ? 'chevron-up' : 'chevron-down'}
-                    color={'#444'}
-                    size={18}
-                  />
-                );
-              }}
-              dropdownIconPosition={'right'}
-              dropdownStyle={styles.dropdown1DropdownStyle}
-              rowStyle={styles.dropdown1RowStyle}
-              rowTextStyle={styles.dropdown1RowTxtStyle}
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        backgroundColor={MyColors.Neutral.NEUTRAL01}
+        barStyle="dark-content"
+      />
+
+      <TouchableOpacity
+        style={styles.backButtonContainer}
+        onPress={() => navigation.goBack()}>
+        <Feather
+          name="arrow-left"
+          size={ms(24)}
+          color={MyColors.Neutral.NEUTRAL00}
+        />
+      </TouchableOpacity>
+
+      <Formik
+        validationSchema={putAccountValidationSchema}
+        initialValues={{fullname: '', address: '', phone_number: ''}}>
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+          isValid,
+        }) => (
+          <>
+            <Text style={styles.inputLabel}>Nama Lengkap</Text>
+            <TextInput
+              name="fullname"
+              style={styles.textInput}
+              placeholder="Nama Lengkap"
+              placeholderTextColor={MyColors.Neutral.NEUTRAL03}
+              selectionColor={MyColors.Primary.DARKBLUE04}
+              value={values.fullname}
+              onChangeText={handleChange('fullname')}
+              onBlur={handleBlur('fullname')}
             />
-            <View style={styles.divider} />
-            <SelectDropdown
-              ref={kotaDropdownRef}
-              data={kota}
-              onSelect={(selectedItem, index) => {
-                console.log(selectedItem, index);
-              }}
-              defaultButtonText={'Pilih Kota'}
-              buttonTextAfterSelection={(selectedItem, index) => {
-                return selectedItem.nama;
-              }}
-              rowTextForSelection={(item, index) => {
-                return item.nama;
-              }}
-              buttonStyle={styles.dropdown2BtnStyle}
-              buttonTextStyle={styles.dropdown2BtnTxtStyle}
-              renderDropdownIcon={isOpened => {
-                return (
-                  <FontAwesome
-                    name={isOpened ? 'chevron-up' : 'chevron-down'}
-                    color={'#444'}
-                    size={18}
-                  />
-                );
-              }}
-              dropdownIconPosition={'right'}
-              dropdownStyle={styles.dropdown2DropdownStyle}
-              rowStyle={styles.dropdown2RowStyle}
-              rowTextStyle={styles.dropdown2RowTxtStyle}
+            {errors.fullname && touched.fullname && (
+              <Text style={styles.errorInput}>{errors.fullname}</Text>
+            )}
+
+            <Gap height={ms(16)} />
+
+            <Text style={styles.inputLabel}>Alamat</Text>
+            <TextInput
+              multiline={true}
+              name="address"
+              style={styles.textArea}
+              placeholder="Contoh: Jalan Ikan Hiu 33"
+              placeholderTextColor={MyColors.Neutral.NEUTRAL03}
+              selectionColor={MyColors.Primary.DARKBLUE04}
+              value={values.address}
+              onChangeText={handleChange('address')}
+              onBlur={handleBlur('address')}
             />
-          </View>
-        </ScrollView>
-      </View>
+            {errors.address && touched.address && (
+              <Text style={styles.errorInput}>{errors.address}</Text>
+            )}
+
+            <Gap height={ms(16)} />
+
+            <Text style={styles.inputLabel}>Nomor Handphone</Text>
+            <TextInput
+              name="phone_number"
+              style={styles.textInput}
+              placeholder="Contoh: 08123456789"
+              placeholderTextColor={MyColors.Neutral.NEUTRAL03}
+              selectionColor={MyColors.Primary.DARKBLUE04}
+              keyboardType="phone-pad"
+              value={values.phone_number}
+              onChangeText={handleChange('phone_number')}
+              onBlur={handleBlur('phone_number')}
+            />
+            {errors.phone_number && touched.phone_number && (
+              <Text style={styles.errorInput}>{errors.phone_number}</Text>
+            )}
+
+            <Gap height={ms(16)} />
+          </>
+        )}
+      </Formik>
+
+      <Dropdown
+        style={[
+          styles.dropdown,
+          isFocus && {borderColor: MyColors.Primary.DARKBLUE04},
+        ]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={provinsi}
+        search
+        maxHeight={300}
+        labelField="nama"
+        valueField="nama"
+        placeholder={!isFocus ? 'Select item' : '...'}
+        searchPlaceholder="Search..."
+        value={provinsi}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={item => {
+          setId(item.id);
+          setValue(item.nama);
+          setIsFocus(false);
+          console.log(item.nama, item.id);
+        }}
+      />
+      <Gap height={ms(16)} />
+      <Dropdown
+        style={[
+          styles.dropdown,
+          isFocus && {borderColor: MyColors.Primary.DARKBLUE04},
+        ]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={kota}
+        search
+        maxHeight={300}
+        labelField="nama"
+        valueField="nama"
+        placeholder={!isFocus ? 'Select item' : '...'}
+        searchPlaceholder="Search..."
+        value={kota}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={item => {
+          setValue(item.nama);
+          setIsFocus(false);
+          console.log(item.nama, item.id);
+        }}
+      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  shadow: {
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 6},
-    shadowOpacity: 0.1,
-    shadowRadius: 10,
-    elevation: 10,
-  },
-  header: {
-    flexDirection: 'row',
-    width,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#F6F6F6',
-  },
-  headerTitle: {color: '#000', fontWeight: 'bold', fontSize: 16},
-  saveAreaViewContainer: {flex: 1, backgroundColor: '#FFF'},
-  viewContainer: {flex: 1, width, backgroundColor: '#FFF'},
-  scrollViewContainer: {
-    flexGrow: 1,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingVertical: '10%',
-  },
-  dropdownsRow: {flexDirection: 'row', width: '100%', paddingHorizontal: '5%'},
-
-  dropdown1BtnStyle: {
+  container: {
     flex: 1,
-    height: 50,
-    backgroundColor: '#FFF',
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#444',
+    padding: ms(16),
+    backgroundColor: MyColors.Neutral.NEUTRAL01,
   },
-  dropdown1BtnTxtStyle: {color: '#444', textAlign: 'left'},
-  dropdown1DropdownStyle: {backgroundColor: '#EFEFEF'},
-  dropdown1RowStyle: {backgroundColor: '#EFEFEF', borderBottomColor: '#C5C5C5'},
-  dropdown1RowTxtStyle: {color: '#444', textAlign: 'left'},
-  divider: {width: 12},
-  dropdown2BtnStyle: {
-    flex: 1,
-    height: 50,
-    backgroundColor: '#FFF',
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#444',
+  backButtonContainer: {
+    marginBottom: ms(24),
   },
-  dropdown2BtnTxtStyle: {color: '#444', textAlign: 'left'},
-  dropdown2DropdownStyle: {backgroundColor: '#EFEFEF'},
-  dropdown2RowStyle: {backgroundColor: '#EFEFEF', borderBottomColor: '#C5C5C5'},
-  dropdown2RowTxtStyle: {color: '#444', textAlign: 'left'},
+  loginTitle: {
+    fontFamily: MyFonts.Bold,
+    fontSize: ms(24),
+    color: MyColors.Neutral.NEUTRAL00,
+    marginBottom: ms(24),
+  },
+  inputContainer: {marginBottom: ms(16), justifyContent: 'center'},
+  inputLabel: {
+    fontFamily: MyFonts.Regular,
+    fontSize: ms(12),
+    color: MyColors.Neutral.NEUTRAL00,
+    marginBottom: ms(4),
+  },
+  textInput: {
+    fontFamily: MyFonts.Regular,
+    maxHeight: ms(48),
+    borderWidth: ms(1),
+    borderColor: MyColors.Neutral.NEUTRAL02,
+    borderRadius: ms(16),
+    paddingVertical: ms(14),
+    paddingHorizontal: ms(16),
+    color: MyColors.Neutral.NEUTRAL00,
+    fontSize: ms(14),
+  },
+  errorInput: {fontFamily: MyFonts.Regular, fontSize: 10, color: 'red'},
+  textArea: {
+    fontFamily: MyFonts.Regular,
+    height: ms(80),
+    maxHeight: ms(100),
+    borderWidth: ms(1),
+    borderColor: MyColors.Neutral.NEUTRAL02,
+    borderRadius: ms(16),
+    paddingVertical: ms(14),
+    paddingHorizontal: ms(16),
+    color: MyColors.Neutral.NEUTRAL00,
+    fontSize: ms(14),
+  },
+  dropdown: {
+    height: ms(48),
+    borderColor: MyColors.Neutral.NEUTRAL02,
+    borderWidth: ms(1),
+    borderRadius: ms(16),
+    paddingVertical: ms(14),
+    paddingHorizontal: ms(16),
+  },
+  placeholderStyle: {
+    fontFamily: MyFonts.Regular,
+    fontSize: ms(14),
+  },
+  selectedTextStyle: {
+    fontFamily: MyFonts.Regular,
+    fontSize: ms(14),
+  },
+  iconStyle: {
+    width: ms(24),
+    height: ms(24),
+  },
+  inputSearchStyle: {
+    fontFamily: MyFonts.Regular,
+    height: ms(40),
+    fontSize: ms(14),
+  },
 });
