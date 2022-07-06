d61de0348e282631c15f27cf53e6d074b1bda9e4
cemilick <cemilick@gmail.com>
2022-07-06 20:29:50 +0700

    move inline style to stylesheet

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -80,82 +80,32 @@ const Index = () => {
         end={{x: 0, y: 1.0}}
         locations={[0, 0.3, 0.5]}
         colors={['#FFE9C9', '#FFF7ED', '#FFF']}
-        style={{
-          width: widthPercentageToDP(100),
-          height: heightPercentageToDP(100),
-        }}>
-        <View style={{alignSelf: 'center', paddingHorizontal: ms(16)}}>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
+        style={styles.linearGradient}>
+        <View style={styles.mainContainer}>
+          <View style={styles.searchContainer}>
             <TextInput
               name="search"
-              style={{
-                backgroundColor: '#FFF',
-                fontSize: ms(14),
-                fontFamily: MyFonts.Regular,
-                height: ms(48),
-                width: ms(328),
-                borderRadius: ms(16),
-                marginTop: ms(38),
-              }}
+              style={styles.searchInput}
               placeholder="Cari di Second Chance"
-              placeholderTextColor={MyColors.Neutral.NEUTRAL03}
-              selectionColor={MyColors.Primary.DARKBLUE04}
+              props={{
+                placeholderTextColor: MyColors.Neutral.NEUTRAL03,
+                selectionColor: MyColors.Primary.DARKBLUE04,
+              }}
               value={keyword}
               onChangeText={text => setKeyword(text)}
             />
           </View>
-          <View
-            style={{
-              flexDirection: 'row',
-              paddingTop: ms(32),
-              justifyContent: 'space-between',
-            }}>
+          <View style={styles.bannerContainer}>
             <View>
-              <Text
-                style={{
-                  fontFamily: MyFonts.Bold,
-                  fontSize: ms(20),
-                  width: ms(180),
-                  color: '#000',
-                }}>
+              <Text style={styles.bannerText}>
                 Bulan Ramadhan Banyak diskon!
               </Text>
-              <Text
-                style={{
-                  fontFamily: MyFonts.Regular,
-                  fontSize: ms(10),
-                  color: '#000',
-                  marginTop: ms(16),
-                }}>
-                Diskon Hingga
-              </Text>
-              <Text
-                style={{
-                  fontFamily: MyFonts.Regular,
-                  color: 'red',
-                  fontSize: ms(18),
-                  marginTop: ms(4),
-                }}>
-                60%
-              </Text>
+              <Text style={styles.discountText}>Diskon Hingga</Text>
+              <Text style={styles.discountPercentage}>60%</Text>
             </View>
-            <Image source={Gift} style={{width: ms(127), height: ms(123)}} />
+            <Image source={Gift} style={styles.banner} />
           </View>
-          <Text
-            style={{
-              fontFamily: MyFonts.Regular,
-              fontSize: ms(14),
-              marginTop: ms(48),
-              color: '#151515',
-              marginBottom: ms(16),
-            }}>
-            Telusuri Kategori
-          </Text>
+          <Text style={styles.categoryHeader}>Telusuri Kategori</Text>
           <FlatList
             data={categories}
             horizontal={true}
@@ -164,7 +114,7 @@ const Index = () => {
                 active={item.name == activeCategory.name ? true : false}
                 type="ctaFilter"
                 filterText={item.name}
-                style={{height: ms(44), marginRight: ms(16)}}
+                style={styles.categoryButton}
                 iconName="search"
                 onPress={() =>
                   setActiveCategory({id: item.id, name: item.name})
@@ -173,7 +123,7 @@ const Index = () => {
             )}
           />
           {product ? (
-            <View style={{flex: 1, marginTop: ms(-300), paddingBottom: ms(50)}}>
+            <View style={styles.cardContainer}>
               <FlatList
                 data={product}
                 numColumns={2}
@@ -184,7 +134,7 @@ const Index = () => {
                       source={item.image_url}
                       price={item.base_price}
                       category={item.Categories}
-                      style={{marginRight: ms(16), marginBottom: ms(16)}}
+                      style={styles.cardProduct}
                     />
                   );
                 }}
@@ -201,4 +151,74 @@ const Index = () => {
 
 export default Index;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  linearGradient: {
+    width: widthPercentageToDP(100),
+    height: heightPercentageToDP(100),
+  },
+  mainContainer: {
+    alignSelf: 'center',
+    paddingHorizontal: ms(16),
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  searchInput: {
+    backgroundColor: '#FFF',
+    fontSize: ms(14),
+    fontFamily: MyFonts.Regular,
+    height: ms(48),
+    width: ms(328),
+    borderRadius: ms(16),
+    marginTop: ms(38),
+  },
+  bannerContainer: {
+    flexDirection: 'row',
+    paddingTop: ms(32),
+    justifyContent: 'space-between',
+  },
+  bannerText: {
+    fontFamily: MyFonts.Bold,
+    fontSize: ms(20),
+    width: ms(180),
+    color: '#000',
+  },
+  discountText: {
+    fontFamily: MyFonts.Regular,
+    fontSize: ms(10),
+    color: '#000',
+    marginTop: ms(16),
+  },
+  discountPercentage: {
+    fontFamily: MyFonts.Regular,
+    color: 'red',
+    fontSize: ms(18),
+    marginTop: ms(4),
+  },
+  banner: {
+    width: ms(127),
+    height: ms(123),
+  },
+  categoryHeader: {
+    fontFamily: MyFonts.Regular,
+    fontSize: ms(14),
+    marginTop: ms(48),
+    color: '#151515',
+    marginBottom: ms(16),
+  },
+  categoryButton: {
+    height: ms(44),
+    marginRight: ms(16),
+  },
+  cardContainer: {
+    flex: 1,
+    marginTop: ms(-300),
+    paddingBottom: ms(50),
+  },
+  cardProduct: {
+    marginRight: ms(16),
+    marginBottom: ms(16),
+  },
+});
