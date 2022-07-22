b932864ba3dcece179670d90230e8a5b8f9679fa
Yohanes Velly Sabattino <yohanesvelly23@gmail.com>
2022-07-22 15:13:31 +0700

    (undone) create action and reducer

--- src/redux/allReducer.js
+++ src/redux/allReducer.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import Global from './globalReducer';
 import LoginReducer from '../screens/Login/redux/reducer';
 import RegisterReducer from '../screens/Register/redux/reducer';
+import UbahAkunReducer from '../screens/UbahAkun/redux/reducer';
 
 export const allReducers = combineReducers({
   login: LoginReducer,
   register: RegisterReducer,
+  ubahAkun: UbahAkunReducer,
   Global,
 });
